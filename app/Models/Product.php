9d070296762f2abe54ba1633f77a62e857bbd594
Elyas Bahodi <10201007@ucn.dk>
2021-11-22 11:08:09 +0100

    Model classes created for product and category

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'productID';
+
+    protected $fillable = ['categoryID', 'name', 'description', 'partNumber', 'price', 'inStock', 'isActive', 'createDate', 'modifiedDate'];
+
+    protected $guarded = ['productID'];
+}
