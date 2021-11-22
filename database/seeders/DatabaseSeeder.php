c2ee9d40e6fffffc09a4ae158ab439a37988f33f
edindk <edindkk@gmail.com>
2021-11-22 11:47:51 +0100

    Igangsat UI delen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(ProductCategorySeeder::class);
-        $this->call(ProductSeeder::class);
+        $this->call([UsersTableSeeder::class]);
+        $this->call([ProductCategorySeeder::class]);
+        $this->call([ProductSeeder::class]);
     }
 }
